9d0989bed0bef233776844d923852adfbaa61040
Maxim Buz <mbuz.maxim@gmail.com>
2022-05-25 15:24:08 +0200

    getting all types

--- gql-folders/cli.js
+++ gql-folders/cli.js
@@ -2,13 +2,21 @@
 
 // grab arguments
 const [, , ...args] = process.argv;
+const [fileName, varName] = args;
 
 // script
 const fs = require("fs");
 
 try {
-  const data = fs.readFileSync(`./${args[0]}`, 'utf8');
-  console.log(data);
+  const data = fs.readFileSync(`./${fileName}`, 'utf8');
+
+  // get full schema
+  const schemaString = data.match(/(typeDef = `)([\s\S]*?)`/g)?.[0]?.replace(`${varName} =`, "")?.replaceAll("`", "");
+  
+  // get all type names
+  const types = schemaString.match(/(?<=type )(.*)(?= {)/g)
+
+  console.log(types);
 } catch (err) {
   console.error(err);
 }
--- gql-folders/test.js
+++ gql-folders/test.js
@@ -1,1 +1,16 @@
-console.log("test");
+export const typeDef = `
+  type Query {
+    name: String!
+    id: Int!
+  }
+
+  type Mutation {
+    name: String!
+    id: Int!
+  }
+
+  type User {
+    name: String!
+    id: Int!
+  }
+`;
