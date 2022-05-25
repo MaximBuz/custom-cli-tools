af6cb6bfbaba6004c2e6ae7a1d54c987d2b1efce
Maxim Buz <mbuz.maxim@gmail.com>
2022-05-25 15:35:33 +0200

    added git ignore and pluralize package

--- gql-folders/cli.js
+++ gql-folders/cli.js
@@ -7,16 +7,29 @@ const [fileName, varName] = args;
 // script
 const fs = require("fs");
 
+let data;
+
+// reading the file
 try {
-  const data = fs.readFileSync(`./${fileName}`, 'utf8');
-
-  // get full schema
-  const schemaString = data.match(/(typeDef = `)([\s\S]*?)`/g)?.[0]?.replace(`${varName} =`, "")?.replaceAll("`", "");
-  
-  // get all type names
-  const types = schemaString.match(/(?<=type )(.*)(?= {)/g)
-
-  console.log(types);
+  data = fs.readFileSync(`./${fileName}`, 'utf8');
 } catch (err) {
   console.error(err);
 }
+
+// get full schema
+const schemaString = data.match(/(typeDef = `)([\s\S]*?)`/g)?.[0]?.replace(`${varName} =`, "")?.replaceAll("`", "");
+
+// get all type names
+const types = schemaString.match(/(?<=type )(.*)(?= {)/g);
+
+try {
+  for (type of types) {
+    const path = `./resolvers/${type.toLowerCase()}s`
+    fs.mkdirSync(path, { recursive: true });
+    fs.writeFileSync(path + "/index.js", "")
+  }
+} catch (err) {
+  console.error(err);
+}
+
+console.log(types);
