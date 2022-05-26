d430fae8943af621c05b59477ecec7faceb25286
Maxim Buz <85324130+MaximBuz@users.noreply.github.com>
2022-05-26 17:22:23 +0200

    Update mutationIndex.js

--- create-react-api-service/mutationIndex.js
+++ create-react-api-service/mutationIndex.js
@@ -1,6 +1,6 @@
 module.exports = queryIndex = () => (
-`export default async function mutation (request) {
-  return fetch(request)
+`export default async function mutation (url, options) {
+  return fetch(url, options)
     .then(res => res.status <= 400 ? res : Promise.reject(res))
     .then(res => res.json())
     .catch(err => console.error(err));
