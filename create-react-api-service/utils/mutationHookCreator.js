2e15fea267082a7e15e93ee70abcb5e3252730e2
Maxim Buz <mbuz.maxim@gmail.com>
2022-05-28 14:10:54 +0200

    fixed mutation hook

--- create-react-api-service/utils/mutationHookCreator.js
+++ create-react-api-service/utils/mutationHookCreator.js
@@ -3,8 +3,10 @@ const name = require("./transformName");
 module.exports = function mutationHookCreator ({ method }) {
   const importsLine = `import mutation from '..';\nimport { ${method} } from '../../endpoints';\n`;
 
-  const hook = `export default function use${name(method)} (id, data) {
-  return mutation(...${method}(id, data));
+  const hook = `export default function use${name(method)} () {
+  return function (id, data) {
+    return mutation(...${method}(id, data));
+  };
 };
 `;
 
